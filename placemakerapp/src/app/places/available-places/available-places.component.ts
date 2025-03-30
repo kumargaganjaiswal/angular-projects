a9de0a74373efaae3e93de8f0dab56382633b55b
ArunKumar-Sharp <arunkumar_sharp@hotmail.com>
2025-03-30 13:05:58 +0530

    Added

---
+++ placemakerapp/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,43 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  isFetching = signal(false);
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const clientRequestSubscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places)
+      )
+      .subscribe({
+        next: (palces) => {
+          this.places.set(palces);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
+      });
+
+    this.destroyRef.onDestroy(() => {
+      clientRequestSubscription.unsubscribe();
+    })
+  }
+}
