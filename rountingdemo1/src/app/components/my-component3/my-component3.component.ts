73064a409efd0dd4b4ece45335cf66ca88d22e2a
ArunKumar-Sharp <arunkumar_sharp@hotmail.com>
2025-04-24 12:19:06 +0530

    updated

--- rountingdemo1/src/app/components/my-component3/my-component3.component.ts
+++ rountingdemo1/src/app/components/my-component3/my-component3.component.ts
@@ -1,4 +1,5 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-component3',
@@ -7,11 +8,16 @@ import { Component, input, OnInit } from '@angular/core';
 })
 export class MyComponent3Component implements OnInit {
   userId = input.required<string>();
-
+  router = inject(Router);
   constructor() { }
 
   ngOnInit() {
     console.log(this.userId());
+  }
+  onBack() {
+    this.router.navigate(['/component2'], {
+      replaceUrl: true
+    });
   }
 
 }
